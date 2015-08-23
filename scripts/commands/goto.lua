fa8e42a37f7916cc32f587fe22c5e48eb8d17f81
Drew Davis <drew@metadavis.com>
2015-08-22 21:47:59 -0600

    Cooldown for @goto command and creation of @gmgoto

--- scripts/commands/goto.lua
+++ scripts/commands/goto.lua
@@ -1,7 +1,7 @@
 ---------------------------------------------------------------------------------------------------
 -- func: goto
--- auth: bluekirby0
--- desc: Goes to the target player.
+-- auth: SwitchVII
+-- desc: Goes to the target player, with a 5 minute cooldown to prevent abuse
 ---------------------------------------------------------------------------------------------------
 
 cmdprops =
@@ -11,15 +11,24 @@ cmdprops =
 };
 
 function onTrigger(player, target)
-    if (target == nil) then
-        player:PrintToPlayer("You must enter a valid player name.");
-        return;
-    end
+	local goto_cooldown = player:getVar("goto_cooldown");
+	local realday = os.time();
+	
+	if (goto_cooldown + 300 < realday) or (goto_cooldown == nil) then
+		if (target == nil) then
+			player:PrintToPlayer("You must enter a valid player name.");
+			return;
+		end
 
-    local targ = GetPlayerByName( target );
-    if (targ ~= nil) then
-        player:setPos( targ:getXPos(), targ:getYPos(), targ:getZPos(), 0, targ:getZoneID() );    
-    else
-        player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
-    end
+		local targ = GetPlayerByName( target );
+		if (targ ~= nil) then
+			player:setVar("goto_cooldown", os.time());
+			player:setPos( targ:getXPos(), targ:getYPos(), targ:getZPos(), 0, targ:getZoneID() );
+		else
+			player:PrintToPlayer( string.format( "Player named '%s' not found!", target ) );
+		end
+	else
+		local timeremaining = 300 - (realday - goto_cooldown);
+		player:PrintToPlayer( string.format( "Goto unavailable for another %s second(s).",  timeremaining) );
+	end
 end
