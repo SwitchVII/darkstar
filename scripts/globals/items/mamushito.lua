cedf3e6eed0484589049a96e2911b4ced290e979
Drew Davis <drew@metadavis.com>
2015-09-21 14:23:57 -0600

    Mamushito official fix

--- scripts/globals/items/mamushito.lua
+++ scripts/globals/items/mamushito.lua
@@ -3,7 +3,6 @@
 -- Item: Mamushito
 -- Additional Effect: Stun
 -----------------------------------------
-
 require("scripts/globals/status");
 require("scripts/globals/magic");
 
@@ -12,13 +11,13 @@ require("scripts/globals/magic");
 -----------------------------------
 
 function onAdditionalEffect(player,target,damage)
-    local chance = 3;
+    local chance = 5;
+
     if (math.random(0,99) >= chance or applyResistanceAddEffect(player,target,ELE_LIGHTNING,0) <= 0.5) then
         return 0,0,0;
     else
-        if (not target:hasStatusEffect(EFFECT_STUN)) then
-            target:addStatusEffect(EFFECT_STUN, 1, 0, 4);
-        end
-        return SUBEFFECT_STUN, 160, EFFECT_STUN;
+        target:addStatusEffect(EFFECT_STUN, 1, 0, 3);
+        return SUBEFFECT_STUN, MSGBASIC_ADD_EFFECT_STATUS, EFFECT_STUN;
     end
 end;
+
--- scripts/globals/items/mamushito_+1.lua
+++ scripts/globals/items/mamushito_+1.lua
@@ -3,7 +3,6 @@
 -- Item: Mamushito +1
 -- Additional Effect: Stun
 -----------------------------------------
-
 require("scripts/globals/status");
 require("scripts/globals/magic");
 
@@ -12,13 +11,13 @@ require("scripts/globals/magic");
 -----------------------------------
 
 function onAdditionalEffect(player,target,damage)
-    local chance = 5;
+    local chance = 10;
+
     if (math.random(0,99) >= chance or applyResistanceAddEffect(player,target,ELE_LIGHTNING,0) <= 0.5) then
         return 0,0,0;
     else
-        if (not target:hasStatusEffect(EFFECT_STUN)) then
-            target:addStatusEffect(EFFECT_STUN, 1, 0, 4);
-        end
-        return SUBEFFECT_STUN, 160, EFFECT_STUN;
+        target:addStatusEffect(EFFECT_STUN, 1, 0, 3);
+        return SUBEFFECT_STUN, MSGBASIC_ADD_EFFECT_STATUS, EFFECT_STUN;
     end
 end;
+
